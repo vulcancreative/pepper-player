fc2e03d081c22dc3b2d4e570b950da36fff0053b
Chris Calo <ccalo@vulcanca.com>
2018-06-04 15:40:48 -0400

    More export tweaks

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
-import Player from './src/player';
-
-export class pepper {
-  constructor() {
-    this.Player = Player;
-  }
-}
-
 export * from './src/player';
 export * from './src/constants';
-export default pepper;
+
+import Player from './src/player';
+export default Player;
