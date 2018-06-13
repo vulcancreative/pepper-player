377ae52849a6854d61599f293cbf5c60980d6c06
Chris Calo <ccalo@vulcanca.com>
2018-06-13 09:15:29 -0400

    Wrote tests for the measure module

--- src/measure.test.js
+++ src/measure.test.js
@@ -1,5 +1,6 @@
 import {
   kbps,
+  speedFactor,
 } from './measure';
 
 describe('kbps', () => {
@@ -8,13 +9,30 @@ describe('kbps', () => {
     const inputB = [44444, 8234];
     const inputC = [123456789, 65432];
 
-    expect(kbps(...inputA)).toBe(0.20);
-    expect(kbps(...inputB)).toBe(0.04);
-    expect(kbps(...inputC)).toBe(14.74);
+    const outputA = 0.20;
+    const outputB = 0.04;
+    const outputC = 14.74;
+
+    expect(kbps(...inputA)).toBe(outputA);
+    expect(kbps(...inputB)).toBe(outputB);
+    expect(kbps(...inputC)).toBe(outputC);
   });
 });
 
 describe('speedFactor', () => {
   it('should convert download speed to meaningful metrics', () => {
+    const inputA = [0.20, 100, 4];
+    const inputB = [0.04, 44444, 8234];
+    const inputC = [14.74, 123456789, 65432];
+
+    const outputA = 976.56;
+    const outputB = 1054.22;
+    const outputC = 1000.04;
+
+    // TODO: add more real-world examples
+
+    expect(speedFactor(...inputA)).toBe(outputA);
+    expect(speedFactor(...inputB)).toBe(outputB);
+    expect(speedFactor(...inputC)).toBe(outputC);
   });
 });
