8bb4151969fadcadf482fed9512e41ce9000c494
Chris Calo <ccalo@vulcanca.com>
2018-09-10 14:19:05 -0400

    Tests written for adp.matchBandwidth

--- src/adp.test.js
+++ src/adp.test.js
@@ -99,6 +99,87 @@ describe('Adp.bestRep', () => {
   });
 });
 
+describe('Adp.bestRep', () => {
+  it('should be capable of identifying the best representation', () => {
+    const inputA = caseA.data;
+    const inputB = caseB.data;
+    const inputC = caseC.data;
+    const inputD = caseD.data;
+
+    /*
+    const outputA = [2, 0];
+    const outputB = [9, 0];
+    const outputC = [9, 0, 0];
+    const outputD = [0, 0];
+    */
+
+    const outputA = ['2', '3'];
+    const outputB = ['bbb_30fps_3840x2160_12000k', 'bbb_a64k'];
+    const outputC = [
+      'bbb_30fps_3840x2160_12000k', 'bbb_a64k', 'thumbnails_320x180'
+    ];
+    const outputD = ['A48', 'V300'];
+
+    expect.assertions(4);
+
+    const promises = [
+      (new MPD({ data: inputA })).setup().then(mpd => {
+        expect(mpd.adps.map(a => a.bestRep().id)).toEqual(outputA);
+      }),
+      (new MPD({ data: inputB })).setup().then(mpd => {
+        expect(mpd.adps.map(a => a.bestRep().id)).toEqual(outputB);
+      }),
+      (new MPD({ data: inputC })).setup().then(mpd => {
+        expect(mpd.adps.map(a => a.bestRep().id)).toEqual(outputC);
+      }),
+      (new MPD({ data: inputD })).setup().then(mpd => {
+        expect(mpd.adps.map(a => a.bestRep().id)).toEqual(outputD);
+      }),
+    ];
+
+    return Promise.all(promises);
+  });
+});
+
+describe('Adp.matchBandwidth', () => {
+  it('should be capable of identifying a nearest-representation', () => {
+    const inputA = caseA.data;
+    const inputB = caseB.data;
+    const inputC = caseC.data;
+    const inputD = caseD.data;
+
+    const outputA = ['0', '3'];
+    const outputB = ['bbb_30fps_640x360_800k', 'bbb_a64k'];
+    const outputC = [
+      'bbb_30fps_640x360_800k', 'bbb_a64k', 'thumbnails_320x180'
+    ];
+    const outputD = ['A48', 'V300'];
+
+    expect.assertions(4);
+
+    const promises = [
+      (new MPD({ data: inputA })).setup().then(mpd => {
+        expect(mpd.adps.map(a => a.matchBandwidth(1000000).id))
+        .toEqual(outputA);
+      }),
+      (new MPD({ data: inputB })).setup().then(mpd => {
+        expect(mpd.adps.map(a => a.matchBandwidth(1000000).id))
+        .toEqual(outputB);
+      }),
+      (new MPD({ data: inputC })).setup().then(mpd => {
+        expect(mpd.adps.map(a => a.matchBandwidth(1000000).id))
+        .toEqual(outputC);
+      }),
+      (new MPD({ data: inputD })).setup().then(mpd => {
+        expect(mpd.adps.map(a => a.matchBandwidth(1000000).id))
+        .toEqual(outputD);
+      }),
+    ];
+
+    return Promise.all(promises);
+  });
+});
+
 /*
 describe('Adp.strongerRep', () => {
   it('should be capable of choosing a stronger representation', () => {
